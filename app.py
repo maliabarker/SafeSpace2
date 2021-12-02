6fcf9cac7534928948c11d817ba5c48934c379cf
maliabarker <89502882+maliabarker@users.noreply.github.com>
2021-12-02 14:02:17 -0800

    added encryption for password and message flashing

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask.helpers import url_for
 from flask_pymongo import pymongo
 from datetime import datetime
 from bson.objectid import ObjectId
+import bcrypt
 
 CONNECTION_STRING = 'mongodb+srv://maliabarker:tpofbawf@safespace.8qywl.mongodb.net/safespace?retryWrites=true&w=majority'
 client = pymongo.MongoClient(CONNECTION_STRING)
@@ -21,25 +22,52 @@ def index():
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    if request.method=='GET':
-        session['email']=request.form['email']
-        user_obj= users.find_one({'email': session['email']})
-        return render_template('home.html', user=user_obj)
-    elif request.method=='POST':
-        avatar = request.form['options']
-        user = {
-            'email':request.form.get('email'),
-            'password':request.form.get('password'),
-            'avatar': 'static/' + 'images/' + avatar + '.png',
-            'username':f'Anonymous {avatar}'
-        }
-        print(user)
-        user = users.insert_one(user)
-        session['email']=request.form['email']
-        user_obj= users.find_one({'email': session['email']})
-        print(user_obj)
+    message = ''
+    if request.method=='POST':
+        email = request.form.get('email')
+        email_found = users.find_one({'email': email})
+
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if email_found:
+            message = 'There is already an account with this email'
+            return render_template('index.html', message=message)
+        elif password1 != password2:
+            message = 'Passwords do not match, please re-enter passwords'
+            return render_template('index.html', message=message)
+        else:
+            avatar = request.form['options']
+            hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
+            user = {
+                'email':request.form.get('email'),
+                'password':hashed,
+                'avatar': 'static/' + 'images/' + avatar + '.png',
+                'username':f'Anonymous {avatar}'
+            }
+            # print(user)
+            users.insert_one(user)
+            session['email']=request.form['email']
+            user_obj= users.find_one({'email': session['email']})
+
+            print(user_obj)
 
         return render_template('home.html', user=user_obj)
+
+    elif request.method=='GET':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        email_found = users.find_one({'email': email})
+        if email_found:
+            email_val = email_found['email']
+            password_check = email_found['password']
+        session['email']=request.form['email']
+        user_obj= users.find_one({'email': session['email']})
+        return render_template('home.html', user=user_obj)
+
+
+
 
 @app.route('/home')
 def home():
@@ -47,6 +75,9 @@ def home():
     post=posts.find()
     print(post)
     return render_template('home.html', user=user_obj, posts=post)
+
+
+
 
 
 @app.route('/<user_id>/home', methods=['POST'])
